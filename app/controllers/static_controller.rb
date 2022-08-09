25b281cbc9bb32626752d8235a40748e1cafb8e1
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-09 15:33:52 +0200

    Add linters

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,8 @@
 class StaticController < ApplicationController
-    def index
-    end
-    def greetings
-        @greetings = Greeting.order(Arel.sql('RANDOM()')).limit(5)
-        render json: @greetings
-    end
+  def index; end
+
+  def greetings
+    @greetings = Greeting.order(Arel.sql('RANDOM()')).limit(5)
+    render json: @greetings
+  end
 end
